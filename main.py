3612878e4845c1f4c1eb0efd88fa227766aae0cc
TigranGithub2 <122722837+TigranGithub2@users.noreply.github.com>
2025-11-15 11:50:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ import time
 
 todo = ["send email to boss" , "homework" , "repair Ps5"]
 
-print("Welcome to the mighty to do list")
+print("Welcome to the mighty to-do list")
 
 while True:
-    print("1 - show my todo's")
-    print("2 - add a new todo")
-    print("3 - delete a todo")
+    print("1 - show my to-do's")
+    print("2 - add a new to-do")
+    print("3 - delete a to-do")
     print("4 - exit")
 
 
@@ -20,28 +20,26 @@ while True:
 
 
     elif action == "2":
-        new_todo = input("insert new todo: ")
+        new_todo = input("insert new to-do: ")
         todo.append(new_todo)
-        print("new todo has been added!")
+        print("new to-do has been added!")
         input("enter to continue!")
 
     elif action == "3":
-        print("todo list: ", todo)
-        remove_todo = input("which word do you want to delete?: ")
+        print("to-do list: ", todo)
+        remove_todo = input("which to-do do you want to delete?: ")
         if remove_todo in todo:
             todo.remove(remove_todo)
             print("deleted!")
             input("enter to continue!")
         else:
-            print("todo was not found!")
+            print("to-do was not found!")
             input("enter to continue!")
             
 
     elif action == "4":
         print("goodbye!")
         quit()
-
-
 
 
 
